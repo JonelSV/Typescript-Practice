a57318626b879e89953015889345352b4ca8e0bc
JonelSV <svjonel@gmail.com>
2021-10-05 21:12:01 -0400

    '--noEmitOnError file.ts'

--- Customer.js
+++ Customer.js
@@ -1,10 +1,15 @@
 var Customer = /** @class */ (function () {
-    function Customer() {
+    function Customer(fName, lName, position, allegiance) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.rank = position;
+        this.loyalty = allegiance;
     }
     return Customer;
 }());
-var myCustomer = new Customer();
-myCustomer.firstName = "Julius Augustus";
-myCustomer.lastName = "Cesar";
-console.log(myCustomer.firstName);
-console.log(myCustomer.lastName);
+var myCustomer = new Customer("Julius", "Cesar", "Imperator", "Roman");
+// myCustomer.firstName = "Julius Augustus";
+// myCustomer.lastName = "Cesar"
+// console.log(myCustomer.firstName);
+// console.log(myCustomer.lastName);
+console.log(myCustomer);
--- Customer.ts
+++ Customer.ts
@@ -6,17 +6,30 @@ class Customer {
     loyalty: string;
 
 
-    constructor ( position: string, allegiance: string) {
+    constructor ( fName: string, lName: string, position: string, allegiance: string) {
+        this.firstName = fName;
+        this.lastName = lName;
         this.rank = position;
         this.loyalty = allegiance;
     }
 
 }
 
-let myCustomer = new Customer();
+let myCustomer = new Customer("Julius", "Cesar", "Imperator", "Devoted Roman");
 
-myCustomer.firstName = "Julius Augustus";
-myCustomer.lastName = "Cesar"
+// myCustomer.firstName = "Julius Augustus";
+// myCustomer.lastName = "Cesar"
 
-console.log(myCustomer.firstName);
-console.log(myCustomer.lastName);
+// console.log(myCustomer.firstName);
+// console.log(myCustomer.lastName);
+
+console.log(myCustomer);
+
+//======================== MODIFIERS ============
+
+// PUBLIC = PROPERTY ACCESSIBLE TO ALL CLASSES
+// PROTECTED = PROPERTY IS ONLY ACCESSIBLE IN CURRENT CLASS AND SUBCLASSES
+// PRIVATE = PROPERTY IS ONLY ACCESSIBLE IN THE CURRENT CLASS.
+
+// TO PREVENT TYPESCRIPT FROM COMPILING WITH ERRORS:
+// USE tsc --noEmitOnError Customer.ts  (this means, do not generate a javascript file if there is a compilation error)
