56bbe33b62c2b8fbc61a804ee6c30687cbdc4f8c
JonelSV <svjonel@gmail.com>
2021-10-06 21:49:04 -0400

    'inheritance2'

--- Driver.js
+++ Driver.js
@@ -1,7 +1,13 @@
 "use strict";
+// import { Soldier } from './Oop'
 Object.defineProperty(exports, "__esModule", { value: true });
-var Oop_1 = require("./Oop");
-var Legionaire = new Oop_1.Soldier("Titus", "Pulo");
-var Legionaire2 = new Oop_1.Soldier("Macius", "Tyranus");
-console.log(Legionaire);
-console.log(Legionaire2);
+// let Legionaire = new Soldier("Titus", "Pulo");
+// let Legionaire2 = new Soldier("Macius", "Tyranus")
+// console.log(Legionaire)
+// console.log(Legionaire2)
+var Shape_1 = require("./Shape");
+var Circle_1 = require("./Circle");
+var myShape = new Shape_1.Shape(10, 15);
+var myCircle = new Circle_1.Circle(5, 10, 20);
+console.log(myShape.getInfo());
+console.log(myCircle.getInfo());
--- Driver.ts
+++ Driver.ts
@@ -1,9 +1,17 @@
-import { Soldier } from './Oop'
+// import { Soldier } from './Oop'
 
 
-let Legionaire = new Soldier("Titus", "Pulo");
-let Legionaire2 = new Soldier("Macius", "Tyranus")
+// let Legionaire = new Soldier("Titus", "Pulo");
+// let Legionaire2 = new Soldier("Macius", "Tyranus")
+
+// console.log(Legionaire)
+// console.log(Legionaire2)
 
 
-console.log(Legionaire)
-console.log(Legionaire2)
+import { Shape } from './Shape';
+import { Circle } from './Circle';
+
+let myShape = new Shape (10, 15);
+let myCircle = new Circle(5,10,20)
+console.log(myShape.getInfo());
+console.log(myCircle.getInfo());
