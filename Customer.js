9c22fcfd30f0f78ef1f6d38489b45e99b2ac93b1
JonelSV <svjonel@gmail.com>
2021-10-06 20:19:23 -0400

    'get/set accessors'

--- Customer.js
+++ Customer.js
@@ -1,15 +1,36 @@
 var Customer = /** @class */ (function () {
-    function Customer(fName, lName, position, allegiance) {
+    function Customer(fName, lName, position, allegiance, age) {
         this.firstName = fName;
         this.lastName = lName;
         this.rank = position;
         this.loyalty = allegiance;
+        this.age = age;
     }
+    // GETTER AND SETTER = HOW TO DEFINE
+    // Getter
+    Customer.prototype.getSecret = function () {
+        return this.secret;
+    };
+    //setter
+    Customer.prototype.setAget = function (age) {
+        this.age = age;
+    };
+    Customer.prototype.getAge = function () {
+        return this.age;
+    };
     return Customer;
 }());
-var myCustomer = new Customer("Julius", "Cesar", "Imperator", "Roman");
+var myCustomer = new Customer("Julius", "Cesar", "Imperator", "Devoted Roman", 66);
+var romanCustomer = new Customer("Brutus", "Junius", "Consul", "Roman", 44);
+console.log(romanCustomer.getAge());
 // myCustomer.firstName = "Julius Augustus";
 // myCustomer.lastName = "Cesar"
 // console.log(myCustomer.firstName);
 // console.log(myCustomer.lastName);
 console.log(myCustomer);
+//======================== MODIFIERS ============
+// PUBLIC = PROPERTY ACCESSIBLE TO ALL CLASSES
+// PROTECTED = PROPERTY IS ONLY ACCESSIBLE IN CURRENT CLASS AND SUBCLASSES
+// PRIVATE = PROPERTY IS ONLY ACCESSIBLE IN THE CURRENT CLASS.
+// TO PREVENT TYPESCRIPT FROM COMPILING WITH ERRORS:
+// USE tsc --noEmitOnError Customer.ts  (this means, do not generate a javascript file if there is a compilation error)
--- Customer.ts
+++ Customer.ts
@@ -5,29 +5,38 @@ class Customer {
     rank: string;
     loyalty: string;
     private secret: string;
+    private age: number;
 
 
-    constructor ( fName: string, lName: string, position: string, allegiance: string) {
+    constructor ( fName: string, lName: string, position: string, allegiance: string, age: number) {
         this.firstName = fName;
         this.lastName = lName;
         this.rank = position;
         this.loyalty = allegiance;
-        
+        this.age = age;
     }
 
     // GETTER AND SETTER = HOW TO DEFINE
-
+    // Getter
     public getSecret(): string {
         return this.secret;
     }
 
+    //setter
+    public setAget(age: number) : void {
+        this.age = age;
+    }
 
-
+    public getAge() : number {
+        return this.age;
+    }
 
 }
 
-let myCustomer = new Customer("Julius", "Cesar", "Imperator", "Devoted Roman");
-let romanCustomer = new Customer("Brutus", "Junius", "Consul", "Roman")
+let myCustomer = new Customer("Julius", "Cesar", "Imperator", "Devoted Roman", 66);
+let romanCustomer = new Customer("Brutus", "Junius", "Consul", "Roman", 44)
+
+console.log(romanCustomer.getAge())
 
 // myCustomer.firstName = "Julius Augustus";
 // myCustomer.lastName = "Cesar"
@@ -45,3 +54,35 @@ console.log(myCustomer);
 
 // TO PREVENT TYPESCRIPT FROM COMPILING WITH ERRORS:
 // USE tsc --noEmitOnError Customer.ts  (this means, do not generate a javascript file if there is a compilation error)
+
+// ========================================
+
+// Typescript has alternate syntax known as ACCESSORS. USES SPECIAL GET/SET METHODS
+// convention is to use leading underscore in property name eg. _firsName: string;
+
+// "set" Accessor has no return type , but "get" has
+// public by default
+
+// get firstName(): string {
+//     return this._firstName;
+// }
+
+// set firstName(value: string): string {
+//     this._firstName = value
+// }
+
+// IF NOT USING ES5 must COMPILER FLAG FOR GET/SET use ==>    tsc --target ES5 customer.ts
+
+// cAN set up a tsconfig.json
+
+// it can store All COMPILER FLAG IN one file for easy use;
+// defines compiler options and project settings. Place file in root directory. Example below
+
+// {
+//     "compilerOptions": {
+//         "noEmitOnError": true,
+//         "target": "es5"
+//     }
+// }
+
+// step above can be done by using console :::: > tsc --init (then edit afterwards)
