223e5dbed3b132e39ddf381806d75caf7bb7adf0
JonelSV <svjonel@gmail.com>
2021-10-07 21:03:52 -0400

    working

--- Driver.ts
+++ Driver.ts
@@ -12,16 +12,16 @@ import { Shape } from './Shape';
 import { Circle } from './Circle';
 import { Rectangle } from './Rectangle'
 
-let myShape = new Shape (10, 15);
+
 let myCircle = new Circle(5,10,20);
 let myRectangle = new Rectangle(0,0,3,7);
-console.log(myShape.getInfo());
+
 console.log(myCircle.getInfo());
 console.log(myRectangle.getInfo());
 
 let theShapes: Shape [] = [];
 
-theShapes.push(myShape);
+
 theShapes.push(myCircle);
 theShapes.push(myRectangle);
 
