71d2ffe65482f5fd9af2fc845910a45ad93ddb4a
JonelSV <svjonel@gmail.com>
2021-10-24 23:07:24 -0400

    'commit'

--- files/app.ts
+++ files/app.ts
@@ -4,17 +4,21 @@ console.log("Squid Game planning")
 // NUMBER
 // STRING 
 
-function add (n1: number, n2: number) {
+function add (n1: number, n2: number, showResult: boolean) {
     if(typeof n1 !== 'number' || typeof n2 !== 'number') {
         throw new Error ('Incorrect input')
+    }
+    if(showResult){
+        console.log("Output will be printed")
     }
     return n1 + n2;
 }
 
 const number1 = 5;
 const number2 = 10;
+const printResult = true;
 
-const result = add(number1, number2)
+const result = add(number1, number2, printResult)
 console.log(result);
 
 // typescript typeof
