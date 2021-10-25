96fb8073a52241f17897896390f2a32a7a16129a
JonelSV <svjonel@gmail.com>
2021-10-24 22:50:37 -0400

    'commit'

--- files/app.ts
+++ files/app.ts
@@ -1,1 +1,15 @@
 console.log("Squid Game planning")
+
+// Typescript types CORE
+// NUMBER
+// STRING 
+
+function add (n1, n2) {
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 10;
+
+const result = add(number1, number2)
+console.log(result);
